bb5f73a5c90a81d81955949217887b06bde723e4
Morgan Pernelle <85439798+morgan-pernelle@users.noreply.github.com>
2022-07-21 16:11:54 +0200

    Add files via upload

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+// == Import
+import './styles.css';
+import Header from '../Header';
+import Intro from '../Intro';
+
+// == Composant
+function App() {
+  return (
+    <>
+      <Header />
+      <Intro />
+    </>
+  );
+}
+
+// == Export
+export default App;
